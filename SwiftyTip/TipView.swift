710bf11599d82a7b8477f78f3901c5ece916a73f
Charlie Hieger <charlie@velosmobile.com>
2020-04-24 13:16:37 -0700

    Basic tip calc with segmented control

--- SwiftyTip/TipView.swift
+++ SwiftyTip/TipView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  TipView.swift
 //  SwiftyTip
 //
 //  Created by Charles Hieger on 4/17/20.
@@ -8,56 +8,67 @@
 
 import SwiftUI
 
-struct TipCalculatorView: View {
+struct TipView: View {
 
-    //@ObservedObject var tipCalculatorViewModel: TipCalculatorViewModel
-
-    @State var billAmountString: String = ""
-    @State var tipPercentageString: String = ""
-    @State var totalAmountString: String = ""
-
-    @State var billAmount: Double = 0
-    @State var tipPercentage: Double = 0
-    @State var totalAmount: Double = 0
-
-//    init() {
-//        tipCalculatorViewModel = TipCalculatorViewModel(billAmountString: $billAmountString, tipPercentageString: $tipPercentageString, totalAmountString: $totalAmountString)
-//    }
+    @ObservedObject var viewModel: TipViewModel
 
     var body: some View {
         VStack(spacing: 20) {
             Image(systemName: "dollarsign.circle")
                 .resizable()
                 .aspectRatio(1, contentMode: .fit)
-                .padding(.horizontal, 100 )
+                .foregroundColor(Color.green)
+                .padding(.horizontal, 80)
                 .padding(.vertical, 20)
-                .background(Color.purple)
-            TextField("$ Bill Amount", text: $billAmountString, onEditingChanged: { _ in
-                self.billAmount. = Double(self.billAmountString)
-                })
-                .font(.system(.largeTitle))
-                .padding(.horizontal, 20)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .keyboardType(.numberPad)
-            TextField("% Tip Percentage", text: $tipPercentageString)
-                .font(.system(.largeTitle))
-                .padding(.horizontal, 20)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .keyboardType(.numberPad)
-            TextField("$ Total Amount", text: $totalAmountString)
-                .font(.system(.largeTitle))
-                .padding(.horizontal, 20)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .keyboardType(.numberPad)
+            HStack {
+                Text(viewModel.currenySymbol)
+                    .foregroundColor(Color.green)
+                TextField("Bill Amount", text: $viewModel.billAmountString)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.decimalPad)
+            }.font(.system(.largeTitle))
+            Picker(selection: $viewModel.tipPercentageIndex, label: Text("Tip Percentage")) {
+                ForEach(0..<viewModel.tipPercentageOptions.count) { index in
+                    Text("\(self.viewModel.tipPercentageOptions[index])%").tag(index)
+                }
+            }.pickerStyle(SegmentedPickerStyle())
+            HStack(spacing: 40) {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Tip")
+                        .foregroundColor(Color.green)
+                    Text("\(viewModel.tipString)")
+                }
+                Color.gray
+                    .frame(width: 1, height: 60)
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Total")
+                        .foregroundColor(Color.green)
+                    Text("\(viewModel.totalString)")
+                }
+            }.font(.system(.largeTitle))
             Spacer()
-
         }
-        .background(Color.blue)
+        .padding(.horizontal, 20)
+        .onTapGesture {
+            self.endEditing()
+        }
+    }
+
+    func endEditing() {
+        UIApplication.shared.endEditing()
     }
 }
 
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+#if DEBUG
 struct TipCalculatorView_Previews: PreviewProvider {
     static var previews: some View {
-        TipCalculatorView()
+        TipView(viewModel: TipViewModel())
     }
 }
+#endif
