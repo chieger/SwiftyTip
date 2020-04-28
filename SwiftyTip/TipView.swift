16b34620bbdd6c0dc6d58eb416c05c7100a1f100
Charlie Hieger <charlie@velosmobile.com>
2020-04-28 11:28:37 -0700

    Add colored background and tap to dismiss keyboard

--- SwiftyTip/TipView.swift
+++ SwiftyTip/TipView.swift
@@ -13,53 +13,61 @@ struct TipView: View {
     @ObservedObject var viewModel: TipViewModel
 
     var body: some View {
-        VStack(spacing: 20) {
-            Image(systemName: "dollarsign.circle")
-                .resizable()
-                .aspectRatio(1, contentMode: .fit)
-                .foregroundColor(Color.green)
-                .padding(.horizontal, 80)
-                .padding(.vertical, 20)
-            HStack {
-                Text(viewModel.currenySymbol)
-                    .foregroundColor(Color.green)
-                TextField("Bill Amount", text: $viewModel.billAmountString)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .keyboardType(.decimalPad)
-            }.font(.system(.largeTitle))
-            Picker(selection: $viewModel.tipPercentageIndex, label: Text("Tip Percentage")) {
-                ForEach(0..<viewModel.tipPercentageOptions.count) { index in
-                    Text("\(self.viewModel.tipPercentageOptions[index])%").tag(index)
+        ZStack {
+            Color.green
+                .edgesIgnoringSafeArea(.all)
+                .onTapGesture {
+                    self.endEditing()
                 }
-            }.pickerStyle(SegmentedPickerStyle())
-            HStack(spacing: 40) {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Tip")
-                        .foregroundColor(Color.green)
-                    Text("\(viewModel.tipString)")
+            VStack(spacing: 20) {
+                Image(systemName: "dollarsign.circle")
+                    .resizable()
+                    .aspectRatio(1, contentMode: .fit)
+                    .foregroundColor(Color.white)
+                    .padding(EdgeInsets(top: 20, leading: 80, bottom: 20, trailing: 80))
+                    .allowsHitTesting(false) // Prevents blocking taps to background view.
+                HStack {
+                    Text(viewModel.currenySymbol)
+                        .foregroundColor(Color.white)
+                    TextField("Bill Amount", text: $viewModel.billAmountString)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.decimalPad)
+                }.font(.system(.largeTitle))
+                Picker(selection: $viewModel.tipPercentageIndex, label: Text("Tip Percentage")) {
+                    ForEach(0..<viewModel.tipPercentageOptions.count) { index in
+                        Text("\(self.viewModel.tipPercentageOptions[index])%").tag(index)
+                    }
                 }
-                Color.gray
-                    .frame(width: 1, height: 60)
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Total")
-                        .foregroundColor(Color.green)
-                    Text("\(viewModel.totalString)")
-                }
-            }.font(.system(.largeTitle))
-            Spacer()
-        }
-        .padding(.horizontal, 20)
-        .onTapGesture {
-            self.endEditing()
+                .pickerStyle(SegmentedPickerStyle())
+                HStack(spacing: 40) {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Tip")
+                        Text("\(viewModel.tipString)")
+                            .bold()
+                    }.foregroundColor(Color.white)
+                    Color.white
+                        .frame(width: 1, height: 60)
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Total")
+                        Text("\(viewModel.totalString)")
+                            .bold()
+                    }.foregroundColor(Color.white)
+                }.font(.system(.largeTitle))
+                Spacer()
+            }
+            .padding(.horizontal, 20)
         }
     }
 
+    /// Relinquishes focus from any text field currently in focus and dismisses the keyboard..
     func endEditing() {
         UIApplication.shared.endEditing()
     }
 }
 
 extension UIApplication {
+
+    /// Prompts the first responder to resign. In the case of a TextField, this triggers the keyoard to be dismissed.
     func endEditing() {
         sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
